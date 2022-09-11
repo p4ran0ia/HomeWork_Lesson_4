c1c6785ece8f00c1581e86ba8612238084157eac
Alexandr <crazy.top@mail.ru>
2022-09-11 20:19:59 +0300

    Task complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input two numbers: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int X = A;
+
+for(int i = 2; i <= B; i++)
+{
+    X = X * A;
+}
+Console.WriteLine(X);
